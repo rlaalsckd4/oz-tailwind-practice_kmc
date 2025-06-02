5b90c1e95f59b65bac3177eff4ac0876dec1f7c7
rlaalsckd4 <kmc060901@gmail.com>
2025-06-02 15:08:42 +0900

    테일윈드 과제 제출

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <main>
       <Header />
-      <section>
+      <section className="flex flex-wrap justify-center gap-[20px] py-[20px] px-[40px]">
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 export default function Header() {
   return (
-    <header className="header-container">
+    <header className="header-container flex justify-between items-center 
+    py-[10px] px-[20px] bg-black">
       <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+      <ul className="menu-container flex justify-center items-center gap-[20px]">
+        <li className="list-none font-normal text-[13px]">로그인</li>
+        <li className="list-none font-normal text-[13px]">회원가입</li>
+        <li className="list-none font-normal text-[13px]">내클래스</li>
       </ul>
     </header>
   );
